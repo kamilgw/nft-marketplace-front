120bff68d66b881c7a271fe2dfb46303ec24e1f8
kamilgw <kamilgw1@gmail.com>
2021-09-11 21:37:18 +0200

    change config

--- front/next.config.js
+++ front/next.config.js
@@ -1,8 +1,5 @@
 const path = require('path')
 const withAntdLess = require('next-plugin-antd-less');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
 
 const config = {
   // optional
@@ -35,4 +32,4 @@ const config = {
   }
 };
 
-module.exports = withBundleAnalyzer(withAntdLess((config)));
+module.exports = withAntdLess((config));
